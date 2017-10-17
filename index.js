e34c37c7299f2edfb0fb13be9aca468cb382055c
Alejandro Romero <alejo8591@gmail.com>
2017-10-17 15:05:49 -0500

    Update one signal

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var OneSignalClient = require('node-onesignal').default;
+var Client = require('node-onesignal').default;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -8,8 +8,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var client_one = new OneSignalClient('570909850918', 'YTJmNGU0NzktNzczNC00ZjI5LWI2ZTgtOGJmNTNlZWMwYzM4');
-  var client_two = new OneSignalClient('8200443519', 'ZjQ2NWM4OTQtOGVmOC00ZDU3LThkNmEtMDVmMzc1Y2QzNmIw');
+  var client_one = new Client('c0eb632b-f498-4a63-a277-9ef5948a5e65', 'YTJmNGU0NzktNzczNC00ZjI5LWI2ZTgtOGJmNTNlZWMwYzM4');
+  var client_two = new Client('012bfcf0-8c09-4b9b-aa35-a47b5c13777a', 'ZjQ2NWM4OTQtOGVmOC00ZDU3LThkNmEtMDVmMzc1Y2QzNmIw');
   client_one.sendNotification('lab11001', {
       included_segments: 'all'
   });
