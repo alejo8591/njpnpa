fa392fc7523fdb0b69fb10d04ef4b9e9b482ef1b
Alejandro Romero <alejo8591@gmail.com>
2017-10-17 14:22:42 -0500

    set message via OneSignal

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var OneSignalClient = require('node-onesignal');
+var client_one = new OneSignalClient('570909850918', 'YTJmNGU0NzktNzczNC00ZjI5LWI2ZTgtOGJmNTNlZWMwYzM4');
+var client_two = new OneSignalClient('8200443519', 'ZjQ2NWM4OTQtOGVmOC00ZDU3LThkNmEtMDVmMzc1Y2QzNmIw');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -7,6 +10,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  client_one.sendNotification('lab11001', {
+      included_segments: 'all'
+  });
+    client_two.sendNotification('lab11002', {
+        included_segments: 'all'
+    });
   response.render('pages/index');
 });
 
